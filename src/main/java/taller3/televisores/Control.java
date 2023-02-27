f2b63161b64dc76c13f5f9264f118665aa51329a
Joan <sebastian9557@gmail.com>
2023-02-26 21:07:18 -0500

    Update Control.java

--- src/main/java/taller3/televisores/Control.java
+++ src/main/java/taller3/televisores/Control.java
@@ -11,11 +11,11 @@ public class Control {
 		
 	}
 	
-	public TV getTV() {
+	public TV getTv() {
 		return tv;
 	}
 	
-	public void setTV(TV tv) {
+	public void setTv(TV tv) {
 		this.tv = tv;
 	}
 	
